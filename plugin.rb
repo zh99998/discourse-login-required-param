1b58ef24597ef328ff7e8268f6f653eeff7d0697
神楽坂玲奈 <zh99998@gmail.com>
2018-06-20 13:47:13 +0800

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,28 @@
+# name: useragent-loginrequired
+# about:  Force login_required on some User Agent.
+# version: 0.1
+# authors: zh99998 <zh99998@gmail.com>
+
+after_initialize do
+  ApplicationController.class_eval do
+    def redirect_to_login_if_required
+      return if current_user || (request.format.json? && is_api?)
+
+      if SiteSetting.login_required? || true #/YGOMobile/.match?(request.user_agent)
+        flash.keep
+
+        if SiteSetting.enable_sso?
+          # save original URL in a session so we can redirect after login
+          session[:destination_url] = destination_url
+          redirect_to path('/session/sso')
+        elsif params[:authComplete].present?
+          redirect_to path("/login?authComplete=true")
+        else
+          # save original URL in a cookie (javascript redirects after login in this case)
+          cookies[:destination_url] = destination_url
+          redirect_to path("/login")
+        end
+      end
+    end
+  end
+end
